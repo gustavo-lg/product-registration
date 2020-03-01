cb7cf90f7710ccf5b2cf37fb889cb406e590a6fc
gustavo-lg <guto_leandro95@hotmail.com>
2020-03-01 16:09:03 -0300

    Finishing basic CRUD

--- src/App.js
+++ src/App.js
@@ -1,20 +1,102 @@
-import React, { Component } from 'react';
-import MyComponent from './MyComponent';
+import React, { Component } from "react";
 import './App.css';
+import ProductItem from './ProductItem';
+import AddProduct from "./AddProduct";
+
+const products = [
+  {
+    name: 'iPad',
+    price: 500
+  },
+  {
+    name: 'iPhone',
+    price: 200
+  }
+];
+
+localStorage.setItem('products',  JSON.stringify(products));
 
 class App extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
 
-  };
+    this.state = {
+      products: JSON.parse(localStorage.getItem('products'))
+    };
 
+    this.onAdd = this.onAdd.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.onEditSubmit = this.onEditSubmit.bind(this);
+  }
+  componentWillMount() {
+    const products = this.getProducts();
+
+    this.setState({products});
+  }
+
+  getProducts(){
+    return this.state.products;
+  }
+
+  onAdd(name, price){
+    const products = this.getProducts();
+
+    products.push({
+      name,
+      price
+    });
+
+    this.setState({products});
+  }
+
+  onDelete(name){
+    const products = this.getProducts();
+
+    const filteredProducts = products.filter(product => {
+      return product.name !== name;
+    });
+
+    this.setState({products: filteredProducts});
+  }
+
+  onEditSubmit(name, price, originalName) {
+    let products = this.getProducts();
+
+    products = products.map(product => {
+      if (product.name === originalName){
+        product.name = name;
+        product.price = price;
+      }
+
+      return product;
+    });
+
+    this.setState({products});
+    
+}
 
   render() {
-    return (
+    return(
       <div className="App">
-        <MyComponent/>
+        <h1>Product Manager</h1>
+
+        <AddProduct
+          onAdd={this.onAdd}
+        />
+
+        {
+          this.state.products.map(product => {
+            return (
+             <ProductItem
+              key={product.name}
+              {...product}
+              onDelete={this.onDelete}
+              onEditSubmit={this.onEditSubmit}
+             />
+            );
+          })
+        }
       </div>
-      
     );
   }
 }
